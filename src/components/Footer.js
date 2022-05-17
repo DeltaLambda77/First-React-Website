d7edded2c88800291f7930b7e4e84a3fcc3f7f05
danlozano77 <danlozano77.gmail.com>
2022-05-17 16:39:13 -0400

    more styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,6 +4,29 @@ export default function Footer() {
 
     return (
         <div className="footer-container">
+            <div className="top-footer-container">
+                <p>
+                    Made using the New York Times API.
+                </p>
+            </div>
+            <div className="bottom-footer-container">
+                <a href="https://www.nytimes.com/">
+                    The New York Times
+                </a>
+                <a href="https://github.com/danlozano77">
+                    My GitHub
+                </a>
+                <a href="https://www.linkedin.com/">
+                    My Linkedin
+                </a>
+                <a href="https://www.twitter.com/">
+                    My Twitter
+                </a>
+                <a href="mailto: danlozano77@gmail.com">
+                    Contact Me
+                </a>
+            </div>
+
 
         </div>
     )
