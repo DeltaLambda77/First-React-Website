3070cb3572cf063e14e548d06262e230f35089bd
danlozano77 <danlozano77.gmail.com>
2022-05-19 21:12:28 -0400

    added props functionality

--- src/components/RandomArticle.js
+++ src/components/RandomArticle.js
@@ -1,9 +1,29 @@
 import React from "react"
 
-export default function RandomArticle() {
+export default function RandomArticle(props) {
     return (
         <div className="random-article-container">
-
+            <p>{props.articleSource}</p>
+            <p>{props.articleLeadParagraph}</p>
+            <p>{props.articleImageURL}</p>
+            <p>{props.articleTitle}</p>
+            <p>{props.articleURL}</p>
+            <p>{props.articleSection}</p>
+            <p>{props.articleSnippet}</p>
+            <p>{props.articlePublishedDate}</p>
+            <p>{props.articleAbstract}</p>
         </div>
     )
 }
+
+/* articleAbstract={newsMonthData.articleAbstract}
+                articleLeadParagraph={newsMonthData.articleLeadParagraph}
+                articleImageURL={newsMonthData.articleImageURL}
+                articleTitle={newsMonthData.articleTitle}
+                articleURL={newsMonthData.articleURL}
+                articleSection={newsMonthData.articleSection}
+                articleSnippet={newsMonthData.articleSnippet}
+                articlePublishedDate={newsMonthData.articlePublishedDate}
+                articleSource={newsMonthData.articleSource}
+
+                */
